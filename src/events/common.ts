1458a62f9c08f1df81d22ef3980ed9fa2188ccbb
Fendy <mgfender07@gmail.com>
2026-05-11 10:49:57 -0400

    Remove unused handler

--- src/events/common.ts
+++
@@ -1,9 +0,0 @@
-import { Discord, On, type ArgsOf, type Client } from "discordx";
-
-@Discord()
-export class Example {
-  @On()
-  messageDelete([message]: ArgsOf<"messageDelete">, client: Client): void {
-    console.log("Message Deleted", client.user?.username, message.content);
-  }
-}
